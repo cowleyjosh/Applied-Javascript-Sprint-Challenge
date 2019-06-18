8afd0b56e1bb6609b555442210490138784c445a
Josh <cowley.josh@gmail.com>
2019-06-17 22:26:17 -0400

    Stetch Goal Carousel completed and functional

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,14 +1,44 @@
 class Carousel {
+    constructor(carouselElement) {
+        this.carouselElement = carouselElement;
+        this.leftButton = carouselElement.querySelector(".carousel .left-button");
+        this.rightButton= carouselElement.querySelector(".carousel .right-button");
+        this.index = 0;
+        this.image = carouselElement.querySelectorAll("img")[this.index];
+        this.image.style.display = "block";
+        this.images = carouselElement.querySelectorAll(".carousel img");
+        this.rightButton.addEventListener("click", () => {
+            this.imageRight();
+        });
+        this.leftButton.addEventListener("click", () => {
+            this.imageLeft();
+        });
+    }
 
+    hide(){
+        this.images.forEach(item => {
+            item.style.display = "none";
+        });
+    }
+
+    show(){
+        this.images[this.index].style.display = "block";
+    }
+    imageRight() {
+        if (this.index <Array.from(this.images).length - 1) {
+            this.index++;
+            this.hide();
+            this.show();
+        }
+    }
+    imageLeft() {
+        if (this.index > 0) {
+            this.index--;
+            this.hide();
+            this.show();
+        }
+    }
 }
 
-let carousel = document.querySelector();
-
-/* If You've gotten this far, you're on your own! Although we will give you some hints:
-    1. You will need to grab a reference to the carousel, and in it grab the left and right buttons
-    2. You will need to grab a reference to all of the images
-    3. Create a current index
-    4. Those buttons are gonna need some click handlers.
-    5. Think of how you would animate this component. Make the cards slide in and out, or fade. It's up to you!
-    6. Have fun!
-*/
+let carousel = document.querySelectorAll(".carousel");
+carousel.forEach(carouselElement => new Carousel(carouselElement)); 
